9b3419bdbc88c1cd655a82b878fce7588aab2dd1
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-01 07:03:57 +0100

    Added last line to check if max in part one

--- src/main/java/se/hernebring/day1/part1/Day1.java
+++ src/main/java/se/hernebring/day1/part1/Day1.java
@@ -9,20 +9,27 @@ import java.util.Scanner;
 import static java.nio.file.Files.readAllLines;
 
 public class Day1 {
+
+    private static int max = 0, current = 0;
+
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/day1sample.txt");
         List<String> lines = readAllLines(file.toPath());
-        int max = 0, current = 0;
         for(String l : lines) {
             if(l.isBlank()) {
-                if(current > max)
-                    max = current;
-
+                addToHighScore();
                 current = 0;
             } else
                 current += Integer.parseInt(l);
 
         }
+        addToHighScore();
         System.out.println(max);
+    }
+
+    private static void addToHighScore() {
+        if(current > max)
+            max = current;
+
     }
 }
