214cc489fdd4921e5f3bd637bdc7b98f35dd7f60
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-04 06:49:23 +0100

    Refactored into methods

--- src/main/java/se/hernebring/day4/part1/Day4.java
+++ src/main/java/se/hernebring/day4/part1/Day4.java
@@ -2,47 +2,51 @@ package se.hernebring.day4.part1;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
 
 import static java.nio.file.Files.readAllLines;
 
 public class Day4 {
+    private static int min1, max1, min2, max2;
+
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/day4samplePart1.txt");
         try(Scanner scanner = new Scanner(file)) {
             int counter = 0;
             while(scanner.hasNext()) {
-                String pair = scanner.nextLine();
-                String[] ranges = pair.split(",");
-                String[] numbers = ranges[0].split("-");
-                int min1 = Integer.parseInt(numbers[0]);
-                int max1 = Integer.parseInt(numbers[1]);
-                int length1 =  max1 - min1;
-                numbers = ranges[1].split("-");
-                int min2 = Integer.parseInt(numbers[0]);
-                int max2 = Integer.parseInt(numbers[1]);
-                int length2 = max2 - min2;
-                boolean inside = true;
-                if(length1 > length2) {
-                    for(int i = min2; i <= max2; i++) {
-                        if(i < min1 | i > max1)
-                            inside = false;
-                    }
-                } else {
-                    for(int i = min1; i <= max1; i++) {
-                        if(i < min2 | i > max2)
-                            inside = false;
-                    }
-                }
+                parseRanges(scanner.nextLine());
+                boolean inside = isSmallerRangeInsideLargerRange();
                 if(inside)
                     counter++;
 
             }
             System.out.println(counter);
         }
+    }
+
+    private static boolean isSmallerRangeInsideLargerRange() {
+        boolean inside = true;
+        if(max1 - min1 > max2 - min2) {
+            for(int i = min2; i <= max2; i++) {
+                if(i < min1 | i > max1)
+                    inside = false;
+            }
+        } else {
+            for(int i = min1; i <= max1; i++) {
+                if(i < min2 | i > max2)
+                    inside = false;
+            }
+        }
+        return inside;
+    }
+
+    private static void parseRanges(String pair) {
+        String[] ranges = pair.split(",");
+        String[] numbers = ranges[0].split("-");
+        min1 = Integer.parseInt(numbers[0]);
+        max1 = Integer.parseInt(numbers[1]);
+        numbers = ranges[1].split("-");
+        min2 = Integer.parseInt(numbers[0]);
+        max2 = Integer.parseInt(numbers[1]);
     }
 }
--- src/main/java/se/hernebring/day4/part2/Day4.java
+++ src/main/java/se/hernebring/day4/part2/Day4.java
@@ -5,38 +5,46 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Day4 {
+    private static int min1 ,max1 ,min2 , max2;
+
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/day4sample.txt");
         try(Scanner scanner = new Scanner(file)) {
             int counter = 0;
             while(scanner.hasNext()) {
-                String pair = scanner.nextLine();
-                String[] ranges = pair.split(",");
-                String[] numbers = ranges[0].split("-");
-                int min1 = Integer.parseInt(numbers[0]);
-                int max1 = Integer.parseInt(numbers[1]);
-                int length1 =  max1 - min1;
-                numbers = ranges[1].split("-");
-                int min2 = Integer.parseInt(numbers[0]);
-                int max2 = Integer.parseInt(numbers[1]);
-                int length2 = max2 - min2;
-                boolean overlaps = false;
-                if(length1 > length2) {
-                    for(int i = min2; i <= max2; i++) {
-                        if(i >= min1 & i <= max1)
-                            overlaps = true;
-                    }
-                } else {
-                    for(int i = min1; i <= max1; i++) {
-                        if(i >= min2 & i <= max2)
-                            overlaps = true;
-                    }
-                }
+                parseRanges(scanner.nextLine());
+                boolean overlaps = doesSmallerRangeTouchLargerRange();
                 if(overlaps)
                     counter++;
 
             }
             System.out.println(counter);
         }
+    }
+
+    private static boolean doesSmallerRangeTouchLargerRange() {
+        boolean overlaps = false;
+        if(max1 - min1 > max2 - min2) {
+            for(int i = min2; i <= max2; i++) {
+                if(i >= min1 & i <= max1)
+                    overlaps = true;
+            }
+        } else {
+            for(int i = min1; i <= max1; i++) {
+                if(i >= min2 & i <= max2)
+                    overlaps = true;
+            }
+        }
+        return overlaps;
+    }
+
+    private static void parseRanges(String pair) {
+        String[] ranges = pair.split(",");
+        String[] numbers = ranges[0].split("-");
+        min1 = Integer.parseInt(numbers[0]);
+        max1 = Integer.parseInt(numbers[1]);
+        numbers = ranges[1].split("-");
+        min2 = Integer.parseInt(numbers[0]);
+        max2 = Integer.parseInt(numbers[1]);
     }
 }
