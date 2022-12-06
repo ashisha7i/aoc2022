95375ac064254613a148c23e0916cee04a461bd8
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-06 07:53:57 +0100

    Lowered i max to j max minus one

--- src/main/java/se/hernebring/day6/StringUtils.java
+++ src/main/java/se/hernebring/day6/StringUtils.java
@@ -12,7 +12,7 @@ public class StringUtils {
     }
 
     private static boolean isUniqueCharacterSequence(String line, int length, int counter) {
-        for(int i = counter - length; i < counter + 1; i++) {
+        for(int i = counter - length; i < counter - 1; i++) {
             for(int j = i + 1; j < counter; j++) {
                 if(line.charAt(i) == line.charAt(j))
                     return false;
