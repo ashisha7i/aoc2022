9b6a1c3d00bbb48158bc42d58957678b310939d4
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-06 06:51:46 +0100

    Refactored out check if unique method

--- src/main/java/se/hernebring/day6/StringUtils.java
+++ src/main/java/se/hernebring/day6/StringUtils.java
@@ -6,18 +6,21 @@ public class StringUtils {
         boolean differentSequence = false;
         while(!differentSequence) {
             counter++;
-            if(counter >= length) {
-                differentSequence = true;
-                for(int i = counter - length; differentSequence && i < counter + 1; i++) {
-                    for(int j = i + 1; j < counter; j++) {
-                        if(line.charAt(i) == line.charAt(j)) {
-                            differentSequence = false;
-                            break;
-                        }
-                    }
-                }
-            }
+            if(counter >= length)
+                differentSequence = isUniqueCharacterSequence(line, length, counter);
+
         }
         return counter;
+    }
+
+    private static boolean isUniqueCharacterSequence(String line, int length, int counter) {
+        for(int i = counter - length; i < counter + 1; i++) {
+            for(int j = i + 1; j < counter; j++) {
+                if(line.charAt(i) == line.charAt(j))
+                    return false;
+
+            }
+        }
+        return true;
     }
 }
